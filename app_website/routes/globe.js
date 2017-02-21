24789bae03b6fdc01485fd6b08ca4f880d82e0e4
JJSeger <jjsbowhunter@yahoo.com>
2017-02-20 23:20:25 -0500

    trying to get the gallery plug in to load
    trying to get the gallery plug in to load

--- app_website/routes/globe.js
+++ app_website/routes/globe.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET globe page. */
 router.get('/', function(req, res, next) {
-    res.render('globe', { title: 'Anthracite Glassworks'});
+    res.render('globe', { title: 'Globe Map'});
 
 });
 

a879302cf8d79c689dbc1210232ef7b2d4c793fe
JJSeger <jjsbowhunter@yahoo.com>
2017-02-22 20:37:15 -0500

    working on getting the feedback page initializing properly
    working on getting the feedback page initializing properly

--- app_website/routes/feedback.js
+++ app_website/routes/feedback.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var feedbackData = require('../data/feedback.json');
 
 
 
-router.get('/', function(req, res, next) {
-    res.render('feedback', { title: 'Anthracite feedback'});
+
+router.get('/api', function(req, res) {
+    res.json('feedbackData');
+
+});
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: false }));
+
+
+
+router.post('/api', function(req, res){
+   feedbackData.unshift(req.body);
+    res.json(feedbackData);
+
 
 });
 
